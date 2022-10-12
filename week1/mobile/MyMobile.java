036aaa8f7f76a15088c20a98c4c57cea5006497d
TestLeafPages <earth@testleaf.com>
2022-10-12 17:55:04 +0530

    Add files via upload

---
+++ week1/mobile/MyMobile.java
@@ -0,0 +1,12 @@
+package mobile;
+
+import week1.day3.Mobile;
+
+public class MyMobile {
+public static void main(String[] args) {
+	Mobile mob=new Mobile();
+	mob.shareDocument();
+	mob.sendMessage();
+	mob.call();
+}
+}
